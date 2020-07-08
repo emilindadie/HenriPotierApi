78c465dac37dd7ffa45001537ca0284a87c86291
Emilin DADIE <dadie.emilin@gmail.com>
2020-07-08 10:51:30 +0200

    init henri potier api with cucumber

---
+++ cucumer.js
@@ -0,0 +1,12 @@
+// cucumber.js
+const common = [
+    'features/**/*.feature', // Specify our feature files
+    '--require-module ts-node/register', // Load TypeScript module
+    '--require test/cucumber/step-definitions/**/*.ts', // Load step definitions
+    '--format progress-bar', // Load custom formatter
+    '--format node_modules/cucumber-pretty' // Load custom formatter
+  ].join(' ');
+  
+  module.exports = {
+    default: common
+  };
