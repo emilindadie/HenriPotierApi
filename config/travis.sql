dc0c007c21e5170a2df7598cedb9c5b51c26e96c
Emilin DADIE <dadie.emilin@gmail.com>
2020-07-09 14:53:07 +0200

    update config file for travis

--- config/travis.sql
+++ config/travis.sql
@@ -3,9 +3,8 @@ CREATE USER 'dev'@'localhost' IDENTIFIED BY 'dev';
 GRANT SELECT,INSERT,UPDATE,DELETE,CREATE,DROP ON *.* TO 'dev'@'localhost';
 # Create DB
 CREATE DATABASE IF NOT EXISTS `henripotier` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `demo`;
+USE `henripotier`;
 # Create Table
-
 CREATE TABLE `card_entity` (
   `id` int(11) NOT NULL,
   `expiration` datetime NOT NULL,
